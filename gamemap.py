ada0ef98eb79f0937433f2222a4a3ff8ab0e6032
Edwin DeNicholas <disruption999@hotmail.com>
2022-03-07 07:04:55 -0800

    Completed tutorial part 3.

--- gamemap.py
+++ gamemap.py
@@ -1,18 +1,17 @@
 import numpy
-from tcod.console import Console
 import tiletype
 
 
 class GameMap:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.tiles = numpy.full((width, height), fill_value=tiletype.floor, order="F")
+        self.tiles = numpy.full((width, height), fill_value=tiletype.wall, order="F")
 
         self.tiles[30:33, 22] = tiletype.wall
 
-    def in_bounds(self, x: int, y: int) -> bool:
+    def in_bounds(self, x, y):
         return 0 <= x < self.width and 0 <= y < self.height
 
-    def render(self, console: Console) -> None:
+    def render(self, console):
         console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
